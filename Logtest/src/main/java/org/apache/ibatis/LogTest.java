ace17c7194a33bce9048001488121b8d894cf568
user <hysrush@naver.com>
2021-09-02 11:32:00 +0900

    로그 추가

---
+++ Logtest/src/main/java/org/apache/ibatis/LogTest.java
@@ -0,0 +1,30 @@
+package org.apache.ibatis;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class LogTest {
+
+	static final Logger logger = LoggerFactory.getLogger(LogTest.class);
+
+
+	@RequestMapping(value = "/test")
+	public String test() throws Exception{
+		
+		logger.trace("trace Level 테스트");
+		logger.debug("debug level 테스트");
+		logger.info("info level 테스트 ");
+		logger.warn("warn level 테스트");
+		logger.error("error level 테스트");
+		
+		return "로그입니다";
+		
+		
+	}
+
+}
